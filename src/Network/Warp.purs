90bdbd82c631c884213378c895424d122e5421a5
Woodson Delhia <woodsondelhia88@gmail.com>
2020-06-28 00:55:08 +0900

    Added function to that parses path also added test

--- src/Network/Warp.purs
+++ src/Network/Warp.purs
@@ -1,8 +1,10 @@
 module Network.Warp 
   ( module Run
   , module Settings 
+  , module Path 
   )
   where
 
-import Network.Warp.Run (runSettings, run) as Run 
-import Network.Warp.Settings (Settings, defaultOnExceptionResponse, defaultSettings) as Settings 
+import Network.Warp.Path (pathInfo) as Path
+import Network.Warp.Run (runSettings, run) as Run
+import Network.Warp.Settings (Settings, defaultOnExceptionResponse, defaultSettings) as Settings
--- test/Main.purs
+++ test/Main.purs
@@ -6,10 +6,12 @@ import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (runSpec)
-import Test.Warp.FileSpec as FileSpec
-import Test.Warp.ServerSpec as ServerSpec
+import Test.Warp.FileSpec as File
+import Test.Warp.PathSpec as Path
+import Test.Warp.ServerSpec as Server
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
-  FileSpec.spec
-  ServerSpec.spec
+  Path.spec
+  File.spec
+  Server.spec
