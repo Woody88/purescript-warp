9b4e2e1c01430551b3bd547ecbece381036b603b
Woodson Delhia <woodsondelhia88@gmail.com>
2020-09-14 17:05:11 +0900

    Added comments for pathinfo

--- src/Network/Warp/Path.purs
+++ src/Network/Warp/Path.purs
@@ -9,6 +9,16 @@ import Data.String (Pattern(..))
 import Data.String as String
 import Network.Wai (Request)
 
+-- | will return the request url as an array of string
+-- | root will return an empty array
+-- |
+-- | > req = Wai.defaulRequest { url = "/hello/world" } :: Request
+-- | > pathInfo req 
+-- | ["hello", "world"] 
+-- | 
+-- | > req = Wai.defaulRequest { url = "/" } :: Request
+-- | > pathInfo req 
+-- | [] 
 pathInfo :: Request -> Array String 
 pathInfo = unwrap >>> _.url >>> pathInfo'
 
