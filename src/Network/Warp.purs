d4d93a1dfc09a1d99e346941ae715af81f5e2f56
Woodson Delhia <woodsondelhia88@gmail.com>
2020-09-19 21:03:48 +0900

    Removed Path module from warp

--- src/Network/Warp.purs
+++ src/Network/Warp.purs
@@ -1,10 +1,8 @@
 module Network.Warp 
   ( module Run
   , module Settings 
-  , module Path 
   )
   where
 
-import Network.Warp.Path (pathInfo) as Path
 import Network.Warp.Run (runSettings, run) as Run
 import Network.Warp.Settings (Settings, defaultOnExceptionResponse, defaultSettings) as Settings
--- test/Main.purs
+++ test/Main.purs
@@ -7,11 +7,9 @@ import Effect.Aff (launchAff_)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (runSpec)
 import Test.Warp.FileSpec as File
-import Test.Warp.PathSpec as Path
 import Test.Warp.ServerSpec as Server
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
-  Path.spec
   File.spec
   Server.spec
