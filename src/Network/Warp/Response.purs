732466f3980ce2a6a9b77ad7fc548862d35b36a7
Robert Porter <robert.porter@rakuten.com>
2021-01-15 15:08:35 +0900

    Performance updates, and purty formatting

--- src/Network/Warp/Response.purs
+++ src/Network/Warp/Response.purs
@@ -1,7 +1,6 @@
-module Network.Warp.Response where 
+module Network.Warp.Response where
 
 import Prelude
-
 import Data.Array ((:))
 import Data.Either (Either(..), note)
 import Data.Foldable (traverse_)
@@ -31,65 +30,78 @@ import Unsafe.Coerce (unsafeCoerce)
 
 -- Response function to be used by `Application`.
 sendResponse :: Settings -> Maybe Buffer -> H.RequestHeaders -> HTTP.Response -> Response -> Aff ResponseReceived
-sendResponse settings _ _ reply (ResponseString status headers data_) = do 
-  let stream = HTTP.responseAsStream reply
+sendResponse = go
+  where
+  go settings buffer requestHeaders reply response = join $ liftEffect $ sendResponse' settings buffer requestHeaders reply response
 
-  liftEffect do 
-    _ <- traverse_ (setHeader $ HTTP.setHeader reply) $ addServerName settings.serverName headers
-    _ <- HTTP.setStatusCode reply status.code 
-    _ <- HTTP.setStatusMessage reply status.message
-    
-    _ <- Stream.writeString stream UTF8 data_ mempty
-    
-    const ResponseReceived <$> (Stream.end stream mempty)
- 
-sendResponse settings _ _ reply (ResponseStream status headers respstream) = do 
-  let stream = HTTP.responseAsStream reply
-  liftEffect do 
-    _ <- traverse_ (setHeader $ HTTP.setHeader reply) $ addServerName settings.serverName headers
-    _ <- HTTP.setStatusCode reply status.code 
-    _ <- HTTP.setStatusMessage reply status.message
-    _ <- Stream.pipe respstream stream
-    const ResponseReceived <$> (Stream.onEnd respstream $ pure unit)
-    
+sendResponse' :: Settings -> Maybe Buffer -> H.RequestHeaders -> HTTP.Response -> Response -> Effect (Aff ResponseReceived)
+sendResponse' settings _ _ reply (ResponseString status headers data_) = do
+  let
+    stream = HTTP.responseAsStream reply
+  HTTP.setStatusCode reply status.code
+  HTTP.setStatusMessage reply status.message
+  traverse_ (setHeader $ HTTP.setHeader reply) $ addServerName settings.serverName headers
+  _ <- Stream.writeString stream UTF8 data_ mempty
+  Stream.end stream mempty
+  pure $ pure ResponseReceived
+
+sendResponse' settings _ _ reply (ResponseStream status headers respstream) = do
+  let
+    stream = HTTP.responseAsStream reply
+  HTTP.setStatusCode reply status.code
+  HTTP.setStatusMessage reply status.message
+  traverse_ (setHeader $ HTTP.setHeader reply) $ addServerName settings.serverName headers
+  _ <- Stream.pipe respstream stream
+  Stream.onEnd respstream mempty
+  pure $ pure ResponseReceived
+
 -- TODO: need to find a better approach than unsafeCoerce
-sendResponse settings rawHeader _ reply (ResponseSocket cb) = do 
-  const ResponseReceived <$> cb (unsafeCoerce reply) rawHeader
+sendResponse' settings rawHeader _ reply (ResponseSocket cb) =
+  pure do
+    cb (unsafeCoerce reply) rawHeader
+    pure ResponseReceived
 
-sendResponse settings rawH reqHead reply (ResponseFile status headers path fpart) = do
-    let sendFile404 = sendResponse settings rawH reqHead reply sendResponseFile404
-        stream = HTTP.responseAsStream reply
-        eFileInfo = note (error "Could not generate fileInfo") <<< mkFileInfo path
+sendResponse' settings rawH reqHead reply (ResponseFile status headers path fpart) =
+  let
+    sendFile404 = sendResponse settings rawH reqHead reply sendResponseFile404
 
-    efileStat <- try $ FSAff.stat path 
-    
-    case efileStat >>= eFileInfo of
-        Left e        -> sendFile404 
-        Right fileInfo -> do 
+    stream = HTTP.responseAsStream reply
+
+    eFileInfo = note (error "Could not generate fileInfo") <<< mkFileInfo path
+  in
+    pure do
+      efileStat <- try $ FSAff.stat path
+      case efileStat >>= eFileInfo of
+        Left e -> sendFile404
+        Right fileInfo -> do
           condReqH <- liftEffect $ condReqHeader reqHead
-          condResH <- liftEffect $ condResHeader headers 
-          case conditionalRequest fileInfo headers condReqH condResH of 
-            WithoutBody s -> liftEffect do 
-              _ <- traverse_ (setHeader $ HTTP.setHeader reply) 
+          condResH <- liftEffect $ condResHeader headers
+          case conditionalRequest fileInfo headers condReqH condResH of
+            WithoutBody s ->
+              liftEffect do
+                _ <-
+                  traverse_ (setHeader $ HTTP.setHeader reply)
                     $ addServerName settings.serverName headers
-              _ <- HTTP.setStatusCode reply s.code 
-              const ResponseReceived <$> (Stream.end stream $ pure unit)  
-            WithBody s h offset len -> liftEffect do 
-              let hdrs = addServerName settings.serverName h
-              _         <- traverse_ (setHeader $ HTTP.setHeader reply) hdrs
-              _         <- HTTP.setStatusCode reply s.code 
-              filestream <- FS.createReadStreamWithRange path offset len 
-              _         <- Stream.pipe filestream stream
-              const ResponseReceived <$> (Stream.onEnd filestream $ pure unit)
+                _ <- HTTP.setStatusCode reply s.code
+                const ResponseReceived <$> (Stream.end stream $ pure unit)
+            WithBody s h offset len ->
+              liftEffect do
+                let
+                  hdrs = addServerName settings.serverName h
+                _ <- traverse_ (setHeader $ HTTP.setHeader reply) hdrs
+                _ <- HTTP.setStatusCode reply s.code
+                filestream <- FS.createReadStreamWithRange path offset len
+                _ <- Stream.pipe filestream stream
+                const ResponseReceived <$> (Stream.onEnd filestream $ pure unit)
 
 addContentLength :: Int -> ResponseHeaders -> ResponseHeaders
 addContentLength l hdrs = (hContentLength /\ show l) : hdrs
 
-addServerName :: String -> ResponseHeaders -> ResponseHeaders 
+addServerName :: String -> ResponseHeaders -> ResponseHeaders
 addServerName name hdrs = (hServer /\ name) : hdrs
 
-setHeader :: (String -> String -> Effect Unit) -> Header -> Effect Unit 
+setHeader :: (String -> String -> Effect Unit) -> Header -> Effect Unit
 setHeader setF (Tuple name val) = setF (unwrap name) val
 
 sendResponseFile404 :: Response
-sendResponseFile404  = ResponseString status404 [hContentType /\ "text/plain; charset=utf-8"] "File not found"
+sendResponseFile404 = ResponseString status404 [ hContentType /\ "text/plain; charset=utf-8" ] "File not found"
